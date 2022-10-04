64606f5b06766a0ae723c47553a86d3d3ca5a4a7
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2022-10-04 15:49:02 +0400

    Remove Handler decorator

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 
-const {Handler} = require('./handler')
 const {SecureEventEmitter} = require('./SecureEventEmitter')
 const {SingularEventEmitter} = require('./singular-event-emitter')
 const {Payload} = require('./payload')
 
 module.exports = {
-    Handler,
     SecureEventEmitter,
     SingularEventEmitter,
     Payload,
